63eb9d0b5cd331b3dd7cac7d14d006e92a91540a
mouftan <elmehdielmansourii@gmail.com>
2023-12-27 07:53:54 +0100

    Update Recon.sh

--- Recon.sh
+++ Recon.sh
@@ -28,7 +28,7 @@ while IFS=$' \t\r\n' read -r domain; do
   subfinder -d "$domain" > "$domain.1.txt"
   amass enum -d "$domain" -config ./config.ini > "$domain.2.txt"
   sort "$domain.1.txt" "$domain.2.txt" > "$domain"
-  nmap -sC -sV -iL "$domain" -oN "$domain.nmap.txt" -v 
+  nmap -F -sC -sV -iL "$domain" -oN "$domain.nmap.txt" -v 
   mv "$domain.1.txt" "$domain.2.txt" "$domain" done/
   mv "$domain.nmap.txt" nmap/
 done < "$DomainList"
